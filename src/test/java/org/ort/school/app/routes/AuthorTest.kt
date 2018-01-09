5c128ef82541a721d376e64e6a441ba7ee5ca008
asm0dey <finkelshtein@domclick.ru>
2018-01-09 18:04:13 +0300

    Starts tests for User area

---
+++ src/test/java/org/ort/school/app/routes/AuthorTest.kt
@@ -0,0 +1,34 @@
+package org.ort.school.app.routes
+
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import com.winterbe.expekt.should
+import org.jooby.Err
+import org.junit.Assert
+import org.junit.Test
+
+import org.pac4j.core.profile.CommonProfile
+
+class AuthorTest {
+
+    @Test
+    fun index() {
+        val profile = mock<CommonProfile>()
+        try {
+            Author().index(profile)
+        } catch (e: Exception) {
+            e.should.be.instanceof(Err::class.java)
+            (e as Err).statusCode().should.be.equal(403)
+            return
+        }
+        Assert.fail("Err has not been thrown")
+    }
+    
+    @Test
+    fun `author should see his page on enter`() {
+        val profile = mock<CommonProfile>()
+        whenever(profile.roles).thenReturn(setOf("author"))
+        val index = Author().index(profile)
+        index.name().should.equal("private/author")
+    }
+}
