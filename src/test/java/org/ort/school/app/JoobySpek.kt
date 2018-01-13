2eedadbaf1e22f23bfba53caf69fc7563b31b3d2
asm0dey <pavel.finkelshtein@gmail.com>
2018-01-14 00:24:11 +0300

    Adds integration test. Adds Email sending itself

--- src/test/java/org/ort/school/app/JoobySpek.kt
+++ src/test/java/org/ort/school/app/JoobySpek.kt
@@ -2,15 +2,31 @@ package org.ort.school.app
 
 import org.jetbrains.spek.api.dsl.SpecBody
 import org.jooby.Jooby
+import java.net.ServerSocket
 
-fun SpecBody.jooby(app: Jooby, body: SpecBody.() -> Unit) {
+fun SpecBody.jooby(app: Jooby, body: SpecBody.(port: Int) -> Unit) {
+    val port = ServerSocket(0).use { it.localPort }
     beforeGroup {
-        app.start("server.join=false")
+        app.start("server.join=false", "application.port=$port")
     }
 
-    body()
+    body(port)
 
     afterGroup {
         app.stop()
     }
 }
+
+fun SpecBody.joobyProd(app: Jooby, body: SpecBody.(port: Int) -> Unit) {
+    val port = ServerSocket(0).use { it.localPort }
+    beforeGroup {
+        app.start("prod", "server.join=false", "application.port=$port")
+    }
+
+    body(port)
+
+    afterGroup {
+        app.stop()
+    }
+}
+
