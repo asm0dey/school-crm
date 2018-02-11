df1621525966265168a435a7775be37eb37f33b2
asm0dey <pavel.finkelshtein@gmail.com>
2018-02-11 16:22:09 +0300

    Updates pom to make app compilable

--- src/test/java/org/ort/school/app/BasicIntegrationTest.kt
+++ src/test/java/org/ort/school/app/BasicIntegrationTest.kt
@@ -6,6 +6,7 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
+import org.jooby.jooby
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
 
