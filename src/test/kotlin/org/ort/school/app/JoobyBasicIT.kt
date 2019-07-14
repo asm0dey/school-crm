b7ae004f90323a84afd5771bf64d849e966666dc
asm0dey <pavel.finkelshtein@gmail.com>
2019-07-14 20:32:01 +0300

    Add second synthetical integration test

--- src/test/kotlin/org/ort/school/app/JoobyBasicIT.kt
+++ src/test/kotlin/org/ort/school/app/JoobyBasicIT.kt
@@ -3,61 +3,80 @@ package org.ort.school.app
 import com.github.kittinunf.fuel.httpGet
 import io.kotlintest.Spec
 import io.kotlintest.shouldBe
-import io.kotlintest.specs.StringSpec
-import org.apache.http.client.fluent.Request
+import io.kotlintest.specs.BehaviorSpec
 import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Testcontainers
 import java.net.ServerSocket
 
 class KPostgreSQLContainer : PostgreSQLContainer<KPostgreSQLContainer>()
 
-@Testcontainers
-abstract class BasicInt : StringSpec() {
+abstract class JoobyIntegrationSpec(body: JoobyIntegrationSpec.(port: Int) -> Unit = {}) : BehaviorSpec() {
 
-    val httpPort = ServerSocket(0).use { it.localPort }
-    val httpsPort = ServerSocket(0).use { it.localPort }
+    private val httpPort = ServerSocket(0).use { it.localPort }
+    private val httpsPort = ServerSocket(0).use { it.localPort }
     private val app = SchoolCRM()
 
     companion object {
         @JvmStatic
-        private var PSQL_CONTAINER: KPostgreSQLContainer = KPostgreSQLContainer()
-
+        private val postgres: KPostgreSQLContainer = KPostgreSQLContainer()
         init {
-            PSQL_CONTAINER.start()
+            postgres.start()
         }
-
     }
 
     override fun beforeSpec(spec: Spec) {
+        super.beforeSpec(spec)
         app.start(
                 "server.join=false",
                 "application.port=$httpPort",
                 "application.securePort=$httpsPort",
-                "db.url=${PSQL_CONTAINER.jdbcUrl}",
-                "db.user=${PSQL_CONTAINER.username}",
-                "db.password=${PSQL_CONTAINER.password}"
+                "db.url=${postgres.jdbcUrl}",
+                "db.user=${postgres.username}",
+                "db.password=${postgres.password}"
         )
     }
 
-    override fun afterSpec(spec: Spec) = app.stop()
+    override fun afterSpec(spec: Spec) {
+        super.afterSpec(spec)
+        app.stop()
+    }
 
     init {
         this.body(httpPort)
     }
-
-    abstract fun body(t: Int)
 }
 
-class JoobyBasicIT : BasicInt() {
-    override fun body(t: Int) {
-        "on get / status should be 200" {
+class JoobyFirstIT : JoobyIntegrationSpec({ port ->
+    given("application started") {
+        `when`("I request /") {
             val response = "http://localhost:$port/"
                     .httpGet()
                     .response()
                     .second
-            response.statusCode shouldBe 200
+
+            then("I get status 200") {
+                response.statusCode shouldBe 200
+            }
+
         }
     }
 
-}
+})
+
+class JoobySecondIT : JoobyIntegrationSpec({ port ->
+    given("application started") {
+        `when`("I request /") {
+            val response = "http://localhost:$port/"
+                    .httpGet()
+                    .response()
+                    .second
+
+            then("I get status 200") {
+                response.statusCode shouldBe 200
+            }
+
+        }
+    }
+
+})
+
 
