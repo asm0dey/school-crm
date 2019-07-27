2c01420e75d0f7e95c07731ee9c5515e5fb85442
asm0dey <pavel.finkelshtein@gmail.com>
2019-07-27 20:21:59 +0300

    mockk tests

---
+++ src/test/kotlin/org/ort/school/app/service/DBAuthKTest.kt
@@ -0,0 +1,20 @@
+package org.ort.school.app.service
+
+import com.winterbe.expekt.should
+import io.kotlintest.shouldThrow
+import io.kotlintest.specs.ShouldSpec
+import io.mockk.every
+import io.mockk.mockk
+import org.ort.school.app.repo.UserRepo
+import org.pac4j.core.credentials.UsernamePasswordCredentials
+import org.pac4j.core.exception.CredentialsException
+
+class DBAuthKTest : ShouldSpec({
+
+    "auth username"{
+    }
+    "auth password"{
+    }
+    "when user not found"{
+    }
+})
--- src/test/kotlin/org/ort/school/app/service/PasswordServiceKTest.kt
+++ src/test/kotlin/org/ort/school/app/service/PasswordServiceKTest.kt
@@ -1,15 +1,8 @@
 package org.ort.school.app.service
 
-import com.winterbe.expekt.should
-import io.kotlintest.matchers.boolean.shouldBeFalse
-import io.kotlintest.matchers.string.shouldContain
-import io.kotlintest.matchers.string.shouldStartWith
-import io.kotlintest.properties.assertAll
-import io.kotlintest.shouldBe
-import io.kotlintest.shouldThrow
-import io.kotlintest.specs.ShouldSpec
 import org.junit.jupiter.api.Assertions.assertNotEquals
 
+//import com.winterbe.expekt.should
 // String Fun Should Feature Behavior
 // styles - encrypted - exception - check - matchers - focus
 class PasswordServiceKTest
