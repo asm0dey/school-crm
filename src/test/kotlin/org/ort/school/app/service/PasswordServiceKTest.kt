c119325312dddd308617b99851bdbd928da08802
asm0dey <pavel.finkelshtein@gmail.com>
2019-09-16 14:38:33 +0300

    Prepare PasswordServiceKTest

--- src/test/kotlin/org/ort/school/app/service/PasswordServiceKTest.kt
+++ src/test/kotlin/org/ort/school/app/service/PasswordServiceKTest.kt
@@ -1,8 +1,17 @@
 package org.ort.school.app.service
 
-import org.junit.jupiter.api.Assertions.assertNotEquals
+import io.kotlintest.data.suspend.forall
+import io.kotlintest.matchers.string.shouldEndWith
+import io.kotlintest.matchers.string.shouldStartWith
+import io.kotlintest.properties.assertAll
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldThrow
+import io.kotlintest.specs.FreeSpec
+import io.kotlintest.specs.ShouldSpec
+import io.kotlintest.tables.row
+import kotlin.test.assertNotEquals
 
 //import com.winterbe.expekt.should
-// String Fun Should Feature Behavior
+// String Fun Should Feature Behavior Free
 // styles - encrypted - exception - check - matchers - focus
 class PasswordServiceKTest
